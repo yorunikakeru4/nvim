726acd794ec9ff9afe020dcb981f141e0baeeaaa
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-11 23:08:34 +0300

    choire: std config

--- modules/plugin-configs/keystroke.nix
+++ modules/plugin-configs/keystroke.nix
@@ -1,25 +1,15 @@
 {...}: {
   programs.nixvim.extraConfigLua = ''
-    local default_sound = vim.api.nvim_get_runtime_file(
-      "lua/keystroke/sound/typewriter/default.ogg",
-      false
-    )[1]
-    local sounds = {
-      [string.char(13)] = vim.api.nvim_get_runtime_file(
-        "lua/keystroke/sound/typewriter/enter.ogg",
-        false
-      )[1],
-    }
-
     require("keystroke").setup({
       auto_start = true,
       handlers = {
         i = {
           sound = {
             enable = true,
-            callback = function(key)
-              vim.system({ "pw-play", sounds[key] or default_sound })
-            end,
+            callback = require("keystroke.sound").play_sound,
+            options = {
+              style = "typewriter",
+            },
           },
         },
         ["*"] = {},
