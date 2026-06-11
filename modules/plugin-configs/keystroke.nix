d99684887b110d2c24a737ac36cf711000a1ab6d
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-11 23:12:13 +0300

    choire: restore pw-play

--- modules/plugin-configs/keystroke.nix
+++ modules/plugin-configs/keystroke.nix
@@ -1,15 +1,34 @@
 {...}: {
   programs.nixvim.extraConfigLua = ''
+    local debounce = require("keystroke.debounce")
+    local default_sound = vim.api.nvim_get_runtime_file(
+      "lua/keystroke/sound/typewriter/default.ogg",
+      false
+    )[1]
+    local sounds = {
+      [string.char(13)] = vim.api.nvim_get_runtime_file(
+        "lua/keystroke/sound/typewriter/enter.ogg",
+        false
+      )[1],
+    }
+    local play_sound = debounce.debounce_trailing(function(path)
+      vim.system({
+        "pw-play",
+        "--channels=2",
+        "--channel-map=Stereo",
+        path,
+      })
+    end, 50, false)
+
     require("keystroke").setup({
       auto_start = true,
       handlers = {
         i = {
           sound = {
             enable = true,
-            callback = require("keystroke.sound").play_sound,
-            options = {
-              style = "typewriter",
-            },
+            callback = function(key)
+              play_sound(sounds[key] or default_sound)
+            end,
           },
         },
         ["*"] = {},
