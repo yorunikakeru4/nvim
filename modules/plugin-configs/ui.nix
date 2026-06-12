46254c019697685b009fc30e7df847706dd89483
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-13 01:30:57 +0300

    choire: in inline

--- modules/plugin-configs/ui.nix
+++ modules/plugin-configs/ui.nix
@@ -19,10 +19,9 @@
                             enabled = true;
                             clear_in_insert_mode = true;
                             download_remote_images = true;
-                            /*
-                              only_render_image_at_cursor = true;
-                              only_render_image_at_cursor_mode = "inline";
-                            */
+                            only_render_image_at_cursor = true;
+                            only_render_image_at_cursor_mode = "inline";
+
                             filetypes = [
                                 "markdown"
                                 "vimwiki"
