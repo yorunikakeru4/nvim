042a7555c41c3633dba73812515fd2d28e6ead98
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-13 22:34:57 +0300

    fix: setup boolean-toggle

--- modules/plugin-configs/editing.nix
+++ modules/plugin-configs/editing.nix
@@ -1,34 +1,42 @@
-{...}: {
-  programs.nixvim.extraConfigLua = ''
-    -- nvim-autopairs extra rules
-    local npairs = require("nvim-autopairs")
-    local Rule  = require("nvim-autopairs.rule")
-    local cond  = require("nvim-autopairs.conds")
-    npairs.add_rules({ Rule("<", ">", { "html", "markdown" }):with_pair():with_move(cond.done()) })
-    npairs.add_rules({ Rule("|", "|", { "rust", "markdown" }):with_pair() })
+{ ... }: {
+    programs.nixvim.extraConfigLua = ''
+        require('boolean-toggle').setup({
+            keymaps = {
+            toggle = '<CR>', -- Toggle on ENTER
+            to_false = '<BS>', -- Set to `false` by pressing Backspace
+            to_true = '<C-BS>', -- Set to `false` by pressing CTRL + Backspace
+            },
+        })
 
-    -- hbac (auto-close old buffers, threshold=10)
-    require("hbac").setup({
-      autoclose = true,
-      threshold = 10,
-    })
+        -- nvim-autopairs extra rules
+        local npairs = require("nvim-autopairs")
+        local Rule  = require("nvim-autopairs.rule")
+        local cond  = require("nvim-autopairs.conds")
+        npairs.add_rules({ Rule("<", ">", { "html", "markdown" }):with_pair():with_move(cond.done()) })
+        npairs.add_rules({ Rule("|", "|", { "rust", "markdown" }):with_pair() })
 
-    -- vim-matchup
-    vim.g.matchup_treesitter_stopline = 500
+        -- hbac (auto-close old buffers, threshold=10)
+        require("hbac").setup({
+          autoclose = true,
+          threshold = 10,
+        })
 
-    -- nvim-surround v4: keymaps are no longer configured via setup().
-    vim.keymap.set("x", "s", "<Plug>(nvim-surround-visual)", {
-      desc = "Add a surrounding pair around a visual selection",
-    })
-  '';
+        -- vim-matchup
+        vim.g.matchup_treesitter_stopline = 500
 
-  programs.nixvim.plugins = {
-    comment.settings = {
-      toggler = {
-        line = "<A-q>";
-        block = "<A-qq>";
-      };
-      opleader.block = "<A-q>";
+        -- nvim-surround v4: keymaps are no longer configured via setup().
+        vim.keymap.set("x", "s", "<Plug>(nvim-surround-visual)", {
+          desc = "Add a surrounding pair around a visual selection",
+        })
+    '';
+
+    programs.nixvim.plugins = {
+        comment.settings = {
+            toggler = {
+                line = "<A-q>";
+                block = "<A-qq>";
+            };
+            opleader.block = "<A-q>";
+        };
     };
-  };
 }
