366b33244b0f2d0251ac1643002432ea79178e5b
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-21 00:43:52 +0300

    fix: raise hlargs priority for rust lsp

--- modules/plugin-configs/ui.nix
+++ modules/plugin-configs/ui.nix
@@ -120,7 +120,8 @@
               paint_arg_declarations = true,
               paint_arg_usages = true,
               paint_catch_blocks = { declarations = false, usages = false },
-              extras = { named_parameters = false, unused_arguments = true },
+              extras = { named_parameters = false, unused_args = true },
+              hl_priority = 130,
               performance = {
                 parse_delay = 1, slow_parse_delay = 50,
                 max_iterations = 400, max_concurrent_partial_parses = 30,
