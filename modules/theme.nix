d4e5fad8ba1e8724e436afd3dc01c1ee757343ac
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-22 15:58:44 +0300

    fix: highligt with italic

--- modules/theme.nix
+++ modules/theme.nix
@@ -27,7 +27,11 @@
               }
 
               for _, group in ipairs(groups) do
-                vim.api.nvim_set_hl(0, group, { italic = true })
+                local ok, highlight = pcall(vim.api.nvim_get_hl, 0, { name = group, link = false })
+                if ok then
+                  highlight.italic = true
+                  vim.api.nvim_set_hl(0, group, highlight)
+                end
               end
             end
 
