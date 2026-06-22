f8e02ff02b9ac0bec431b5e1f917cd0e1b5932ca
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-22 15:55:11 +0300

    choire: italic keygroups

--- modules/theme.nix
+++ modules/theme.nix
@@ -8,6 +8,35 @@
             };
         };
 
+        extraConfigLua = ''
+            local function set_treesitter_keyword_italics()
+              local groups = {
+                "@keyword",
+                "@keyword.coroutine",
+                "@keyword.function",
+                "@keyword.operator",
+                "@keyword.import",
+                "@keyword.type",
+                "@keyword.modifier",
+                "@keyword.repeat",
+                "@keyword.return",
+                "@keyword.debug",
+                "@keyword.exception",
+                "@conditional",
+                "@repeat",
+              }
+
+              for _, group in ipairs(groups) do
+                vim.api.nvim_set_hl(0, group, { italic = true })
+              end
+            end
+
+            set_treesitter_keyword_italics()
+
+            vim.api.nvim_create_autocmd("ColorScheme", {
+              callback = set_treesitter_keyword_italics,
+            })
+        '';
     };
 
 }
