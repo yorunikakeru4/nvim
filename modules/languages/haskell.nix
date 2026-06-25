4fd50c2d9f23c9d982541ff49e4d654710217639
Alexandr_Croitor <kroitorsasha06@gmail.com>
2026-06-25 21:34:36 +0300

    delete: other shit

--- modules/languages/haskell.nix
+++ modules/languages/haskell.nix
@@ -1,36 +1,44 @@
 {
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
-  cfg = config.nixvimLanguages.haskell;
-in {
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [haskell-language-server fourmolu haskellPackages.cabal-fmt];
+    lib,
+    pkgs,
+    config,
+    ...
+}:
+let
+    cfg = config.nixvimLanguages.haskell;
+in
+{
+    config = lib.mkIf cfg.enable {
+        home.packages = with pkgs; [
+            haskell-language-server
+            fourmolu
+            haskellPackages.cabal-fmt
+        ];
 
-    programs.nixvim.plugins.lsp.servers.hls = {
-      enable = true;
-      installGhc = false;
-      cmd = ["haskell-language-server-wrapper" "--lsp"];
-      filetypes = ["haskell" "lhaskell"];
-      rootMarkers = ["*.cabal" "stack.yaml" "cabal.project" "package.yaml" "hie.yaml" ".git"];
-      settings.haskell = {
-        formattingProvider = "fourmolu";
-        checkProject = true;
-        plugin.stan.globalOn = true;
-      };
+        programs.nixvim.plugins.lsp.servers.hls = {
+            enable = true;
+            installGhc = false;
+            cmd = [
+                "haskell-language-server-wrapper"
+                "--lsp"
+            ];
+            filetypes = [
+                "haskell"
+                "lhaskell"
+            ];
+            rootMarkers = [
+                "*.cabal"
+                "stack.yaml"
+                "cabal.project"
+                "package.yaml"
+                "hie.yaml"
+                ".git"
+            ];
+            settings.haskell = {
+                formattingProvider = "fourmolu";
+                checkProject = true;
+                plugin.stan.globalOn = true;
+            };
+        };
     };
-
-    programs.nixvim.extraConfigLua = ''
-      -- haskell-scope-highlighting (patch nil scope_matches bug upstream)
-      local _hs_ts = require("haskell-scope-highlighting.treesitter")
-      local _orig_caps = _hs_ts.captures_at_point_sorted
-      _hs_ts.captures_at_point_sorted = function(...)
-        local bufnr, matches = _orig_caps(...)
-        return bufnr, matches or {}
-      end
-      require("haskell-scope-highlighting").setup({})
-    '';
-  };
 }
